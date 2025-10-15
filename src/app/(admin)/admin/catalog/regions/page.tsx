d95aff45a7607a47f5061482b12c1b5ed1542857
Jasmyne Jean-Remy <jasmynej14@gmail.com>
2025-10-15 14:21:28 -0400

    table regions

--- src/app/(admin)/admin/catalog/regions/page.tsx
+++ src/app/(admin)/admin/catalog/regions/page.tsx
@@ -11,8 +11,30 @@ export default function RegionCatalogPage() {
             .then(res => setRegions(res.data))
     }, []);
     return (
-        <div>
-            <h1>Regions</h1>
+        <div className="w-full p-5">
+            <h1 className="text-2xl">Regions</h1>
+            <div className="w-3/4 p-2">
+                <table className="w-full border-collapse text-left">
+
+                    <thead>
+                    <tr className="text-xl">
+                        <th className="p-2">Name</th>
+                        <th className="p-2">Type</th>
+                        <th className="p-2">Region</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {regions.map((region) => (
+                        <tr key={region.id} className="text-lg">
+                            <td className="p-2">{region.name}</td>
+                            <td className="p-2">{region.type}</td>
+                            <td className="p-2">{region.parent?.name ?? "-"}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     )
 }
--- src/repo/catalog.ts
+++ src/repo/catalog.ts
@@ -3,7 +3,11 @@ import {Prisma, Region, PrismaClient, Country, Destination} from "@prisma/client
 const prisma = new PrismaClient()
 
 async function getAllRegions(): Promise<Region[]> {
-    return prisma.region.findMany()
+    return prisma.region.findMany({
+        include: {
+            parent: true
+        }
+    })
 }
 
 async function getAllCountries(): Promise<Country[]>{
